b83cfde8511c8748582760d961f03aa5d7a0ac4e
xavierflorensa <xavier.florensa@yahoo.com>
2024-01-05 19:21:27 +0100

    Added a second textbox to display response from server

--- ProjectFiles/NetSolution/RuntimeNetLogic1.cs
+++ ProjectFiles/NetSolution/RuntimeNetLogic1.cs
@@ -21,9 +21,10 @@ public class RuntimeNetLogic1 : FTOptix.NetLogic.BaseNetLogic
 
     [ExportMethod]
     
-    public void Message_send(NodeId textboxNodeId)
+    public void Message_send(NodeId textboxNodeId,NodeId textboxNodeId2 )
     {
         var textbox = InformationModel.Get<TextBox>(textboxNodeId);
+        var textbox2 = InformationModel.Get<TextBox>(textboxNodeId2);
         Log.Info("A button has been pressed");
         textbox.FontSize = 40;
         var missatge = textbox.Text;
@@ -34,5 +35,11 @@ public class RuntimeNetLogic1 : FTOptix.NetLogic.BaseNetLogic
         stream.Write(sendData, 0, sendData.Length);
         //Console.WriteLine("sending data to server...");
         Log.Info("sending data to server...");
+        StreamReader sr = new StreamReader(stream);
+        string response = sr.ReadLine();
+        //Console.WriteLine(response);
+        Log.Info(response);
+        textbox2.FontSize = 40;
+        textbox2.Text = response;
     }
  }
